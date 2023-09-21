c77607f7ac10c5f5ae4b3f51ae28202d5765c94b
404ErrorBrainNotFound <130075292+404ErrorBrainNotFound@users.noreply.github.com>
2023-09-21 21:15:21 +0530

    Problem Statement 5
    Write a program to remove all characters from a string except alphabets in a given string.

---
+++ q5.cs
@@ -0,0 +1,20 @@
+using System;
+namespace C-Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your a string: ");
+            string str = Console.ReadLine();
+            string newstr= "";
+            for(int i=0;i<str.Length;i++)
+            {
+                char c= str[i];
+                if (Char.IsLetter(c))
+                    newstr += c;
+            }
+            Console.WriteLine("Old String: "+str+"\nNew String: "+newstr);
+        }
+    }
+}
